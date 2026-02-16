46b5c77654f2d2e446c33d0c31eea4cef1a36b03
MrXisaCyborg <jaadi5497@gmail.com>
2026-02-16 15:43:12 +0530

    UC1: Application Entry & Welcome Message

---
+++ PalindromeCheckerApp1/src/PalindromeCheckerApp.java
@@ -0,0 +1,15 @@
+/*
+  This application is about palindrome checking
+  This stage includes start of the app
+  The goal is to develop a smooth application using java
+  @author Aadi Jain
+  @version 1.0
+*/
+
+public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Palindrome Checker System");
+        System.out.println("Version 1.0");
+        System.out.println("System Initialized Successfully.");
+    }
+}
