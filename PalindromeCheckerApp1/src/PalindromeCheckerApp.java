7e7321772ec25fb04eb48aaea4dece5f0f410894
K-Ayaan <kunwarayaan9655@gmail.com>
2026-03-10 15:00:24 +0530

    UC10 Case insensitive and space ignored palindrome checker

--- PalindromeCheckerApp1/src/PalindromeCheckerApp.java
+++ PalindromeCheckerApp1/src/PalindromeCheckerApp.java
@@ -1,59 +1,21 @@
-public class PalindromeCheckerApp {
+public class UseCase10PalindromeCheckerApp {
 
-    // Node class for linked list
-    static class Node {
-        char data;
-        Node next;
+    public static boolean isPalindrome(String word) {
 
-        Node(char data) {
-            this.data = data;
-            this.next = null;
-        }
-    }
+        // Normalize string: remove spaces and convert to lowercase
+        word = word.replaceAll("\\s+", "").toLowerCase();
 
-    // Reverse linked list
-    public static Node reverse(Node head) {
-        Node prev = null;
-        Node current = head;
-        Node next = null;
+        int start = 0;
+        int end = word.length() - 1;
 
-        while (current != null) {
-            next = current.next;
-            current.next = prev;
-            prev = current;
-            current = next;
-        }
+        while (start < end) {
 
-        return prev;
-    }
-
-    // Check palindrome
-    public static boolean isPalindrome(Node head) {
-
-        if (head == null || head.next == null) {
-            return true;
-        }
-
-        Node slow = head;
-        Node fast = head;
-
-        // Find middle
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-
-        // Reverse second half
-        Node secondHalf = reverse(slow);
-        Node firstHalf = head;
-
-        // Compare halves
-        while (secondHalf != null) {
-            if (firstHalf.data != secondHalf.data) {
+            if (word.charAt(start) != word.charAt(end)) {
                 return false;
             }
-            firstHalf = firstHalf.next;
-            secondHalf = secondHalf.next;
+
+            start++;
+            end--;
         }
 
         return true;
@@ -61,18 +23,9 @@ public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        String word = "madam";
+        String word = "Race Car";
 
-        // Convert string to linked list
-        Node head = new Node(word.charAt(0));
-        Node current = head;
-
-        for (int i = 1; i < word.length(); i++) {
-            current.next = new Node(word.charAt(i));
-            current = current.next;
-        }
-
-        if (isPalindrome(head)) {
+        if (isPalindrome(word)) {
             System.out.println(word + " is a palindrome.");
         } else {
             System.out.println(word + " is not a palindrome.");
